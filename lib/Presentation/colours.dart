ab38b5b4bbe4881bc24b8c94dea7fdb3ab4c7281
dean-nema <nemarambakundayi@gmail.com>
2025-01-12 21:06:43 +0200

    fixed height restriction and improved colour
    made the homePage a scrollable widget then changed the pink colour to dark colour

--- lib/Presentation/colours.dart
+++ lib/Presentation/colours.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Colours {
-  static Color appBarBackground = Colors.purpleAccent.shade400;
+  static Color appBarBackground = const Color.fromARGB(255, 92, 53, 99);
   static Color mainBackground = Colors.white;
   static Color secondBackground = Colors.blue[900]!;
   static Color fourthBackground = Colors.brown[300]!;
 
   static Color thirdBackground = Colors.blue[100]!;
-  static Color tableIconColor = Colors.purpleAccent.shade400;
-  static Color display = Colors.purpleAccent.shade100;
+  static Color tableIconColor = const Color.fromARGB(255, 92, 53, 99);
+  static Color display = const Color.fromARGB(255, 92, 53, 99);
   static Color textColor = Colors.white;
   static Color mainDark = Colors.grey[300]!;
   static Color mainDark2 = Colors.cyan[700]!;
--- lib/Presentation/home_page.dart
+++ lib/Presentation/home_page.dart
@@ -42,84 +42,86 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colours.appBarBackground,
       ),
       backgroundColor: Colours.mainBackground,
-      body: Column(
-          children: [
-            //Table Button
-            Padding(
-              padding: const EdgeInsets.only(top: 20.0, bottom: 10),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        width: tableButtonWidth,
-                        height: 50,
-                        child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const Recordings()),
-                              );
-                            },
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Icon(
-                                  Icons.table_chart,
-                                  color: Colours.tableIconColor,
-                                ),
-                                mobile
-                                    ? SizedBox()
-                                    : Text(
-                                        "View Records",
-                                        style: FontStyles.main,
-                                      )
-                              ],
-                            )),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.only(top: 20.0, bottom: 20),
-              child: Container(
-                width: width - 50,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(22),
-                    color: Colours.display),
-                child: Center(
-                  child: Text(
-                    "Today",
-                    style: FontStyles.otherTitles,
+      body: SingleChildScrollView(
+        child: Column(
+            children: [
+              //Table Button
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0, bottom: 10),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          width: tableButtonWidth,
+                          height: 50,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const Recordings()),
+                                );
+                              },
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  Icon(
+                                    Icons.table_chart,
+                                    color: Colours.tableIconColor,
+                                  ),
+                                  mobile
+                                      ? SizedBox()
+                                      : Text(
+                                          "View Records",
+                                          style: FontStyles.main,
+                                        )
+                                ],
+                              )),
+                        ),
+                      )
+                    ],
                   ),
                 ),
               ),
-            ),
-
-            //Room row
-            RowDisplay(
-              title: "Room",
-            ),
-            //Fridge row
-            Padding(
-              padding: heightController
-                  ? const EdgeInsets.only(top: 20.0)
-                  : heightController2
-                      ? const EdgeInsets.only(top: 100)
-                      : const EdgeInsets.only(top: 150),
-              child: RowDisplay(
-                title: "Fridge",
+        
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0, bottom: 20),
+                child: Container(
+                  width: width - 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(22),
+                      color: Colours.display),
+                  child: Center(
+                    child: Text(
+                      "Today",
+                      style: FontStyles.otherTitles,
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ],
-        )
+        
+              //Room row
+              RowDisplay(
+                title: "Room",
+              ),
+              //Fridge row
+              Padding(
+                padding: heightController
+                    ? const EdgeInsets.only(top: 20.0)
+                    : heightController2
+                        ? const EdgeInsets.only(top: 100)
+                        : const EdgeInsets.only(top: 150),
+                child: RowDisplay(
+                  title: "Fridge",
+                ),
+              ),
+            ],
+          ),
+      )
       
     );
   }
